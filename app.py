0f1aed1d16d4066dcc4412ab88c4e98cb710b558
ninadsumant <ninadssumant@gmail.com>
2020-01-01 15:58:20 +0530

    minor changes

--- app.py
+++ app.py
@@ -18,6 +18,10 @@ def executive():
 def mentalstrength():
     return render_template('mentalstrength.html')
 
+@app.route('/breath')
+def breath():
+    return render_template('breath.html')
+
 
 if __name__ == '__main__':
   app.run(debug=True)

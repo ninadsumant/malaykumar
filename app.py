6451c4bca18828fb811cf620464d5761075aac26
ninadsumant <ninadssumant@gmail.com>
2020-02-09 16:15:17 +0530

    monir changes

--- app.py
+++ app.py
@@ -40,10 +40,6 @@ def breath():
 def meditate():
     return render_template('meditation.html')
 
-@app.route('/mindrelaxer')
-def mindrelaxer():
-    return render_template('mindrelaxer.html')
-
 @app.route('/mood')
 def mood():
     return render_template('mood.html')
